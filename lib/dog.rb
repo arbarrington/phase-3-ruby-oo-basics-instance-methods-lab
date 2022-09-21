134fffeea11751df1ba72af5de7a1ddb3b4e655c
Austin Barrington <arbarrington12@gmail.com>
2022-09-21 08:58:37 -0600

    commit tree 330f1c06b6c6d6b4101aff98a1ff3f08bd015a3c

---
+++ lib/dog.rb
@@ -0,0 +1,23 @@
+require 'pry'
+
+class Dog
+    def bark
+        puts "Woof!" 
+    end
+    def sit
+        puts "The Dog is sitting" 
+    end
+end
+
+fido = Dog.new
+snoopy = Dog.new
+
+snoopy.bark
+
+
+
+
+
+
+
+
